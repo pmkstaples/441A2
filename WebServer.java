98c1e626680cbf49c1ca1214c54bee5361034657
Paul Staples <pmkstaples@gmail.com>
2017-10-16 23:04:42 -0600

    Code formatting

--- WebServer.java
+++ WebServer.java
@@ -85,34 +85,34 @@ public class WebServer extends Thread {
      * A simple driver.
      */
     public static void main(String[] args) {
-	int serverPort = 2225;
+    	int serverPort = 2225;
 	
-	// parse command line args
-	if (args.length == 1) {
-	    serverPort = Integer.parseInt(args[0]);
-	}
+    	// parse command line args
+    	if (args.length == 1) {
+    		serverPort = Integer.parseInt(args[0]);
+    	}
 	
-	if (args.length >= 2) {
-	    System.out.println("wrong number of arguments");
-	    System.out.println("usage: WebServer <port>");
-	    System.exit(0);
-	}
+    	if (args.length >= 2) {
+    		System.out.println("wrong number of arguments");
+    		System.out.println("usage: WebServer <port>");
+    		System.exit(0);
+    	}
 	
-	System.out.println("starting the server on port " + serverPort);
+    	System.out.println("starting the server on port " + serverPort);
 	
-	WebServer server = new WebServer(serverPort);
+    	WebServer server = new WebServer(serverPort);
 	
-	server.start();
-	System.out.println("server started. Type \"quit\" to stop");
-	System.out.println(".....................................");
+    	server.start();
+    	System.out.println("server started. Type \"quit\" to stop");
+    	System.out.println(".....................................");
 	
-	Scanner keyboard = new Scanner(System.in);
-	while ( !keyboard.next().equals("quit") );
+    	Scanner keyboard = new Scanner(System.in);
+    	while ( !keyboard.next().equals("quit") );
 	
-	System.out.println();
-	System.out.println("shutting down the server...");
-	server.shutdown();
-	keyboard.close();
-	System.out.println("server stopped");
+    	System.out.println();
+    	System.out.println("shutting down the server...");
+    	server.shutdown();
+    	keyboard.close();
+    	System.out.println("server stopped");
     }
 }
